271dc2c371a7914de03043b358a2bf94deeba7e0
Flooorent <florent.moiny@gmail.com>
2021-01-24 18:43:56 +0100

    fix correc python_requires field in setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='3.7.6',
+    python_requires='==3.7.6',
     install_requires=required,
 )
